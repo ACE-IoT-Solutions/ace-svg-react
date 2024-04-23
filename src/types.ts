38948d12f9c70c4641985a3732dfa8bbd1b70420
nicfv <github@nicfv.com>
2024-04-23 16:31:03 -0700

    Remove autocomplete (enabled always)

--- src/types.ts
+++ src/types.ts
@@ -6,11 +6,8 @@ export interface ACESVGOptions {
   readonly captureMappings: boolean;
   readonly addAllIDs: boolean;
   readonly svgSource: string;
-  readonly svgAutocomplete: boolean;
   readonly eventSource: string;
-  readonly eventAutocomplete: boolean;
   readonly initSource: string;
-  readonly initAutocomplete: boolean;
   svgMappings: SVGIDMapping[];
 }
 export interface ACESVGDefaults {
