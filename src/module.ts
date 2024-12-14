138ab2c2d6c88917d7a8a665d018211120d19586
Nic <github@nicfv.com>
2024-12-14 14:35:44 -0800

    Start messing around, everything is broken

--- src/module.ts
+++ src/module.ts
@@ -1,14 +1,20 @@
 import { PanelPlugin } from '@grafana/data';
-import { SimpleOptions } from './types';
-import { SimplePanel } from './components/SimplePanel';
+import { ACESVGOptions } from './types';
+import { SimplePanel } from './components/Panel';
+import { CodeEditor } from '@grafana/ui';
 
-export const plugin = new PanelPlugin<SimpleOptions>(SimplePanel).setPanelOptions((builder) => {
+export const plugin = new PanelPlugin<ACESVGOptions>(SimplePanel).setPanelOptions((builder) => {
   return builder
-    .addTextInput({
-      path: 'text',
-      name: 'Simple text option',
-      description: 'Description of panel option',
-      defaultValue: 'Default value of text input option',
+    .addCustomEditor({
+      category: ['SVG Document'],
+      path: 'svgSource',
+      name: 'SVG Source',
+      description: 'Editor for SVG Document, while small tweaks can be made here, we recommend using a dedicated graphical SVG Editor and simply pasting the resulting XML here.',
+      id: 'svgSource',
+      defaultValue: '',
+      editor(props) {
+        return <CodeEditor/>;
+      },
     })
     .addBooleanSwitch({
       path: 'showSeriesCount',
