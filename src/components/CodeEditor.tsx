5ee56f71768469cd4985a5b10d46458f1ff45fae
Nic <github@nicfv.com>
2024-12-21 11:32:53 -0800

    Add CodeEditor

---
+++ src/components/CodeEditor.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import { StandardEditorProps } from '@grafana/data';
+import { CodeEditor } from '@grafana/ui';
+
+/**
+ * Settings for the source code editor.
+ */
+export interface SourceCodeSettings {
+    /**
+     * The language for syntax highlighting.
+     */
+    readonly language: string;
+};
+
+/**
+ * The properties object structure for the source code editor.
+ */
+type SourceCodeProps = StandardEditorProps<string, SourceCodeSettings>;
+
+/**
+ * The source code editor React functional component.
+ */
+export const SourceCode: React.FC<SourceCodeProps> = (props) => <CodeEditor value={props.value} language={props.item.settings?.language ?? ''} onBlur={props.onChange} height={'10rem'} showLineNumbers={true} />;
