4c29e7a4c62ec43d6a6fe2bbf5acd0190b151d8e
Nic <github@nicfv.com>
2024-12-21 13:00:22 -0800

    Set defaults

--- src/module.ts
+++ src/module.ts
@@ -1,7 +1,8 @@
 import { PanelPlugin } from '@grafana/data';
 import { ACESVGOptions } from './types';
 import { ACESVGPanel } from './components/ACESVGPanel';
-import { SourceCode, SourceCodeSettings } from 'components/CodeEditor';
+import { SourceCode, SourceCodeSettings } from './components/CodeEditor';
+import { defaults } from './defaults';
 
 export const plugin = new PanelPlugin<ACESVGOptions>(ACESVGPanel).useFieldConfig().setPanelOptions((builder) => {
   return builder
@@ -11,6 +12,7 @@ export const plugin = new PanelPlugin<ACESVGOptions>(ACESVGPanel).useFieldConfig
       name: 'SVG Source',
       description: 'Editor for SVG Document, while small tweaks can be made here, we recommend using a dedicated Graphical SVG Editor and simply pasting the resulting XML here.',
       category: ['SVG Document'],
+      defaultValue: defaults.svgSource,
       settings: {
         language: 'xml',
       },
@@ -22,6 +24,7 @@ export const plugin = new PanelPlugin<ACESVGOptions>(ACESVGPanel).useFieldConfig
       name: 'User JS Initialization Code',
       description: 'The User JS Init code is executed once when the panel loads, you can use this to define helper functions that you later reference in the User JS Render code section. The sections have identical execution contexts, and any JS objects you want to reference between them will need to be attached to the options object as properties.',
       category: ['User JS Initialization'],
+      defaultValue: defaults.initSource,
       settings: {
         language: 'javascript',
       },
@@ -33,6 +36,7 @@ export const plugin = new PanelPlugin<ACESVGOptions>(ACESVGPanel).useFieldConfig
       name: 'User JS Render Code',
       description: 'The User JS Render code is executed whenever new data is available, the root svg document is available as `svgnode`, and elements you\'ve mapped using the SVG Mapping tools below are available as properties on the `svgmap` object. The Grafana DataFrame is provided as `data` and the `options` object can be used to pass values and references between the Render context and the Init context.',
       category: ['User JS Render'],
+      defaultValue: defaults.eventSource,
       settings: {
         language: 'javascript',
       },
@@ -43,13 +47,13 @@ export const plugin = new PanelPlugin<ACESVGOptions>(ACESVGPanel).useFieldConfig
       name: 'Add all SVG element IDs',
       description: 'Parse the SVG Document for Elements with IDs assigned and automatically add them to the mapping list.',
       category: ['SVG Mapping'],
-      defaultValue: false,
+      defaultValue: defaults.addAllIDs,
     })
     .addBooleanSwitch({
       path: 'captureMappings',
       name: 'Enable SVG Mapping on Click',
       description: 'When activated, clicking an element in the panel will attempt to map the clicked element or its nearest parent element with an ID assigned.',
       category: ['SVG Mapping'],
-      defaultValue: false,
+      defaultValue: defaults.captureMappings,
     });
 });
