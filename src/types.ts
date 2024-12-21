18690eab40f283a0ab3964af5c18230c4d6919d9
Nic <github@nicfv.com>
2024-12-21 12:23:18 -0800

    Add global types

--- src/types.ts
+++ src/types.ts
@@ -1,7 +1,42 @@
-type SeriesSize = 'sm' | 'md' | 'lg';
-
-export interface SimpleOptions {
-  text: string;
-  showSeriesCount: boolean;
-  seriesCountSize: SeriesSize;
+/**
+ * An SVG ID mapping.
+ */
+export interface SVGIDMapping {
+  /**
+   * The ID property of the SVG element.
+   */
+  readonly svgId: string;
+  /**
+   * The mapped name of the SVG element.
+   */
+  readonly mappedName: string;
+}
+/**
+ * Options for this panel.
+ */
+export interface ACESVGOptions {
+  /**
+   * Enable capture on click?
+   */
+  readonly captureMappings: boolean;
+  /**
+   * Add all SVG element IDs?
+   */
+  readonly addAllIDs: boolean;
+  /**
+   * The SVG source code.
+   */
+  readonly svgSource: string;
+  /**
+   * The user JS initialization source code.
+   */
+  readonly initSource: string;
+  /**
+   * The user JS render source code.
+   */
+  readonly eventSource: string;
+  /**
+   * The list of SVG mappings.
+   */
+  svgMappings: SVGIDMapping[];
 }
