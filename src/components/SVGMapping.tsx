d02443c8af0f99df4e691cfec1f0d47941b3cdd1
nicfv <github@nicfv.com>
2025-01-04 15:51:31 -0800

    Remove tooltips from inputs, which affects onBlur

--- src/components/SVGMapping.tsx
+++ src/components/SVGMapping.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Stack, Input, Tooltip } from '@grafana/ui';
+import { Stack, Input } from '@grafana/ui';
 import { SVGIDMapping } from 'types';
 
 interface SVGMappingProps {
@@ -14,30 +14,26 @@ export class SvgMapping extends React.PureComponent<SVGMappingProps, SVGIDMappin
   public render(): React.JSX.Element {
     return (
       <Stack>
-        <Tooltip content={'This is the SVG ID attribute value, found from \`<element id="example">\` in the SVG source.'}>
-          <Input
-            type='text'
-            defaultValue={this.props.value.svgId}
-            placeholder='SVG ID'
-            onInput={(e) => {
-              const newState: SVGIDMapping = { svgId: e.currentTarget.value, mappedName: this.state?.mappedName ?? this.props.value.mappedName };
-              this.setState(newState);
-              this.props.onChange(newState);
-            }}
-          />
-        </Tooltip>
-        <Tooltip content={'The user-defined mapped name is accessed by \`svgmap.example\` in the user JavaScript code.'}>
-          <Input
-            type='text'
-            defaultValue={this.props.value.mappedName}
-            placeholder='Mapped name'
-            onInput={(e) => {
-              const newState: SVGIDMapping = { svgId: this.state?.svgId ?? this.props.value.svgId, mappedName: e.currentTarget.value };
-              this.setState(newState);
-              this.props.onChange(newState);
-            }}
-          />
-        </Tooltip>
+        <Input
+          type='text'
+          defaultValue={this.props.value.svgId}
+          placeholder='SVG ID'
+          onBlur={(e) => {
+            const newState: SVGIDMapping = { svgId: e.currentTarget.value, mappedName: this.state?.mappedName ?? this.props.value.mappedName };
+            this.setState(newState);
+            this.props.onChange(newState);
+          }}
+        />
+        <Input
+          type='text'
+          defaultValue={this.props.value.mappedName}
+          placeholder='Mapped name'
+          onBlur={(e) => {
+            const newState: SVGIDMapping = { svgId: this.state?.svgId ?? this.props.value.svgId, mappedName: e.currentTarget.value };
+            this.setState(newState);
+            this.props.onChange(newState);
+          }}
+        />
       </Stack>
     );
   }
