6fe76296bc2b175aa15d7371ae4c3206db6f3e52
nicfv <github@nicfv.com>
2025-01-05 11:19:37 -0800

    Add docs

--- src/components/SVGMappings.tsx
+++ src/components/SVGMappings.tsx
@@ -9,27 +9,37 @@ import { SVGMappingItem } from './SVGMappingItem';
  * The React Component for the entire list of SVG mappings.
  */
 export class SvgMappings extends React.PureComponent<PanelOptionsEditorProps<SVGIDMapping[]>> {
-
+  /**
+   * Adds a new SVG mapping.
+   */
   private onAdd(mapping: SVGIDMapping): void {
     this.props.onChange([...this.props.value, mapping]);
   }
-
+  /**
+   * Updates an existing SVG mapping.
+   */
   private onChange(index: number, mapping: SVGIDMapping): void {
     const mappings: SVGIDMapping[] = [...this.props.value];
     mappings[index] = mapping;
     this.props.onChange(mappings);
   }
-
+  /**
+   * Deletes an existing SVG mapping.
+   */
   private onDelete(index: number): void {
     const mappings: SVGIDMapping[] = [...this.props.value];
     mappings.splice(index, 1);
     this.props.onChange(mappings);
   }
-
+  /**
+   * Clears the entire list of SVG mappings.
+   */
   private onClear(): void {
     this.props.onChange([]);
   }
-
+  /**
+   * Render the editor component.
+   */
   public render(): React.JSX.Element {
     return (
       <Stack direction={'column'}>
